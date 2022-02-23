739cf78408937fa9647ce4b065f35d844fa73b3e
Kevin Gallo <Gallok731@gmail.com>
2022-02-23 17:04:18 -0500

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+
+
+    def initialize(word)
+        @word=word
+    end
+
+    def match(arr)
+        new_arr = []
+        word_arr = @word.chars
+        arr.each {|word| 
+
+            if word.chars.sort == word_arr.sort
+                new_arr << word
+            end
+        }
+        return new_arr
+    end
+end
